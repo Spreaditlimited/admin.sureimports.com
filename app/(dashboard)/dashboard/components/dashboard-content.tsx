37969d31678245171abe897d87c036f774b827e2
Emmanuel Atsu <zioncodex@gmail.com>
2025-02-12 19:23:50 +0100

    UPDATE: Major Admin Update11

--- app/(dashboard)/dashboard/components/dashboard-content.tsx
+++ app/(dashboard)/dashboard/components/dashboard-content.tsx
@@ -23,25 +23,25 @@ interface StatCardProps {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           <StatCard
             title="Customers"
-            value={0}
+            value={(36327).toLocaleString("en-US") as any}
             subtitle="All Registered Customers"
             className="bg-blue-50 dark:bg-blue-950"
           />
           <StatCard
             title="Active Customers"
-            value={0}
+            value={(1231).toLocaleString("en-US") as any}
             subtitle="Active Customers"
             className="bg-yellow-50 dark:bg-yellow-950"
           />
-          <StatCard title="Orders" value={0} subtitle="Active Orders" className="bg-green-50 dark:bg-green-950" />
-          <StatCard title="Products" value={0} subtitle="All Products" className="bg-red-50 dark:bg-red-950" />
+          <StatCard title="Orders" value={(67).toLocaleString("en-US") as any} subtitle="Active Orders" className="bg-green-50 dark:bg-green-950" />
+          <StatCard title="Products" value={(46).toLocaleString("en-US") as any} subtitle="All Products" className="bg-red-50 dark:bg-red-950" />
           <StatCard
-            title="Categories"
-            value={0}
-            subtitle="All Product Categories"
+            title="Services"
+            value={(5).toLocaleString("en-US") as any}
+            subtitle="All Services"
             className="bg-green-50 dark:bg-green-950"
           />
-          <StatCard title="Payments" value={0} subtitle="All Transactions" className="bg-red-50 dark:bg-red-950" />
+          <StatCard title="Payments" value={(5).toLocaleString("en-US") as any} subtitle="All Transactions" className="bg-red-50 dark:bg-red-950" />
         </div>
       </>
     )
