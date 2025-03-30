0462ae4654a1472363cd4481dada1aa083f0db84
Emmanuel Atsu <zioncodex@gmail.com>
2025-03-30 18:07:15 +0100

    FEATURE: New Store Feature17

--- app/(dashboard)/dashboard/store/add/components/AddProduct.tsx
+++ app/(dashboard)/dashboard/store/add/components/AddProduct.tsx
@@ -233,7 +233,7 @@ const Page = () => {
             name='productMOQ' 
             type="number" 
             placeholder="Provide Minimum Order Quantity" 
-            defaultValue={1}
+            //defaultValue={1}
             onChange={(e) => setProductMOQ(e.target.value)}
             required
             className="mt-1 block w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 bg-white dark:bg-gray-700 text-gray-900 dark:text-white"
